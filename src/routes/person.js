bf690234435cbbf2eb145af75efc2133345511b6
Dcruz05 <dcruz11@ucol.mx>
2023-03-28 13:48:02 -0600

    Creacion de index.js en routes, modificacion server.js,comentarios

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,8 @@
 let express = require('express');
-let router = express.Router();
+let router = express.Router(); //usamos export.Router para solo usar router para hacer ruteo
 
 router.get('/person',(req, res) => {
-    res.send("Haz solicitado el listado de personas")
+    res.send("Haz solicitado el listado de personas") // para verificar que se esta mandando bien la informacion usamos person
 });
 
-module.exports = router;
+module.exports = router; //exportamos el router para poder acceder a la ruta en server.js
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 let express=require('express'); //importamos express
 
 //Rutas
+let indexRoute = require('./routes/index');
 let personsRoute = require('./routes/person');//definimos una variable para usar nuestra ruta que se encuentra en otra carpeta
 let studentRoute = require('./routes/student');
 let addStudentRoute = require('./routes/addStudent');
@@ -12,21 +13,16 @@ let port=process.env.PORT || 3000; //
 app.use('/assets', express.static(__dirname+'/public'));
 app.use(express.urlencoded({extended: false})) // Esta linea de codigo hara que nos permita pasear en caso de no tenerla marcara error al momento de querer mostrar resultados
 app.set('view engine', 'ejs'); // Usamos el motor de vistas ejs para poder mostrar nuestras views
-
-//Redireccion
-app.use(personsRoute); // Llamamos a la variable que seleccionamos para mostrar una route
-
 app.use('/',(req, res, next) => {
     console.log('Request URL:' + req.url);
     next()
 });
 
-app.get('/',  (req, res) => {
-    res.render('index');
-});
-
+//Redireccion
+app.use(indexRoute) //Index Route
+app.use(personsRoute); // Llamamos a la variable que seleccionamos para mostrar una route (TEST)
 app.use(studentRoute); // llamamos el rotuer de student y addStudent para poder usarlos desde nuestro server
-app.use(addStudentRoute);
+app.use(addStudentRoute); //displayData route
 
 app.listen(port); // Inicamos el server
 
